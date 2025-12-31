8351c6f8afef2507a95886e124a6c4ffb7682c7b
buikimtrai <buikimtrai2k5@gmail.com>
2025-12-31 16:28:21 +0700

    Update lai code moi nhat

--- QLGiaoHang_API/QLGiaoHang_API/Shared/DTOs/ApiResponse.cs
+++ QLGiaoHang_API/QLGiaoHang_API/Shared/DTOs/ApiResponse.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Azure.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace Shared.DTOs
 {
-    internal class ApiResponse
+    public class ApiResponse
     {
+        public bool Success {  get; set; }
+        public string Message { get; set; }
+        public object Data {  get; set; }   
+        public static ApiResponse Ok(object data, string message = "Thành công")
+        {
+            return new ApiResponse { Success = true, Message = message, Data = data };
+        }
+
+        public static ApiResponse Fail(string message)
+        {
+            return new ApiResponse { Success=false, Message = message, Data = null };
+        }
     }
 }
--- QLGiaoHang_API/QLGiaoHang_API/Shared/Helpers/SqlHelper.cs
+++ QLGiaoHang_API/QLGiaoHang_API/Shared/Helpers/SqlHelper.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Helpers
 {
-    internal class SqlHelper
+    public class SqlHelper
     {
+        private readonly string _connectionString;
+        public SqlHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // 1. Hàm lấy dữ liệu --> trả về DataTable
+        public DataTable ExecuteQuery(string procedureName, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        // 2.Hàm ExecuteNonQuery --> Số dòng bị ảnh hường
+        public int ExecuteNonQuery(string procedureName, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // 3. Hàm lấy 1 giá trị duy nhất ví dụ: Count, 
+        public object ExcuteScala(string procedureName, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
     }
 }
