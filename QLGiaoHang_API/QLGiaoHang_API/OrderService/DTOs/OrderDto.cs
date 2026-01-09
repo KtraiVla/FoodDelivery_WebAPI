3fb9eec6ffb257798493f79b7133d6a63829d6f0
buikimtrai <buikimtrai2k5@gmail.com>
2026-01-09 20:49:58 +0700

    Them cac api cho khach hang

--- QLGiaoHang_API/QLGiaoHang_API/OrderService/DTOs/OrderDto.cs
+++ QLGiaoHang_API/QLGiaoHang_API/OrderService/DTOs/OrderDto.cs
@@ -15,5 +15,31 @@
             public string PhuongThucThanhToan { get; set; } = "TienMat"; // mặc định là tiền mặt
             public List<CartItemDto>  GioHang { get; set; } 
         }
+    
+    // DTO để hiện thị dánh sách đơn hàng 
+    public class OrderResponseDto
+    {
+        public int MaDonHang { get; set; }
+        public DateTime NgayDat { get; set; }
+        public string TrangThai { get; set; }
+        public decimal TongTien { get; set; }
+        public string TenNhaHang { get; set; } // Dành cho khách xem
+        public string DiaChiGiao { get; set; }
+        public string PhuongThucThanhToan { get; set; }
+
+        // Thông tin cho Shipper/Nhà hàng xem
+        public string? TenKhachHang { get; set; }
+        public string? SoDienThoai { get; set; }
+        public string? DiaChiLay { get; set; }
+    }
+
+
+    // DTO cho thống kê
+    public class OrderStatsDto
+    {
+        public int TongDon { get; set; }
+        public decimal DoanhThu { get; set; }
+        public int DonHuy { get; set; }
+    }
 
 }
--- QLGiaoHang_API/QLGiaoHang_API/OrderService/Interfaces/IOrderService.cs
+++ QLGiaoHang_API/QLGiaoHang_API/OrderService/Interfaces/IOrderService.cs
@@ -5,6 +5,23 @@ namespace OrderService.Interfaces
 {
     public interface IOrderService
     {
-        (int ResultCode, string Message, int OrderId) CreateOrder(int userId, CreateOrderRequest request);
+        // KHÁCH 
+        (int ResultCode, string Message, int OrderId) CreateOrder(int userId, CreateOrderRequest request);    // Tạo đơn hàng mới
+        List<OrderResponseDto> GetMyOrders(int userId, string? statusFilter);    // xem lịch sử đặt hàng
+        (bool Success, string Message) CancelOrder(int userId, int orderId);   // hủy đơn hàng
+
+        // NHÀ HÀNG & ADMIN
+        List<OrderResponseDto> GetMerchantOrders(int userId); // userId của nhân viên -> tìm ra quán
+        List<OrderResponseDto> GetAllOrders(); // Admin
+        (bool Success, string Message) UpdateOrderStatus(int orderId, string newStatus);
+        OrderStatsDto GetStats();
+
+        // SHIPPER
+        List<OrderResponseDto> GetPendingOrdersForShipper();
+        (bool Success, string Message) ShipperReceiveOrder(int userId, int orderId);
+        List<OrderResponseDto> GetShipperTasks(int userId);
+        (bool Success, string Message) ShipperCompleteOrder(int userId, int orderId);
+
     }
 }
+   
